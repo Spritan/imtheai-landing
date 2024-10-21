2e33c1c7b1d6c60d7fd2f54ef51c2fdeceee2122
Parjan <parjanhussain@gmail.com>
2024-10-22 02:58:57 +0530

    oo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,9 +131,9 @@ export default async function Home() {
         <span className="bg-slate-600 px-2 text-white">Gallery</span>
       </h1>
 
-      <section className="bg-slate-100 grainy-dark py-24">
-        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
-          {/* <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+      <section className="bg-slate-100 grainy-dark">
+        {/* <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32"> */}
+        {/* <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               What our{" "}
               <span className="relative px-2">
@@ -145,16 +145,16 @@ export default async function Home() {
             <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div> */}
 
-          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
-            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
-              {/* <div className="flex gap-0.5 mb-2">
+        {/* <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16"> */}
+        <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+          {/* <div className="flex gap-0.5 mb-2">
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
               </div> */}
-              {/* <div className="text-lg leading-8">
+          {/* <div className="text-lg leading-8">
                 <p>
                   "The case feels durable and I even got a compliment on the
                   design. Had the case for two and a half months now and{" "}
@@ -165,7 +165,7 @@ export default async function Home() {
                   yellow-ish color after a couple weeks. Love it."
                 </p>
               </div> */}
-              {/* <div className="flex gap-4 mt-2">
+          {/* <div className="flex gap-4 mt-2">
                 <img
                   className="rounded-full h-12 w-12 object-cover"
                   src="/users/user-1.png"
@@ -179,18 +179,18 @@ export default async function Home() {
                   </div>
                 </div>
               </div> */}
-            </div>
+        </div>
 
-            {/* second user review */}
-            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
-              {/* <div className="flex gap-0.5 mb-2">
+        {/* second user review */}
+        <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+          {/* <div className="flex gap-0.5 mb-2">
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
                 <Star className="h-5 w-5 text-slate-600 fill-slate-600" />
               </div> */}
-              {/* <div className="text-lg leading-8">
+          {/* <div className="text-lg leading-8">
                 <p>
                   "I usually keep my phone together with my keys in my pocket
                   and that led to some pretty heavy scratchmarks on all of my
@@ -202,7 +202,7 @@ export default async function Home() {
                   . I dig it."
                 </p>
               </div> */}
-              {/* <div className="flex gap-4 mt-2">
+          {/* <div className="flex gap-4 mt-2">
                 <img
                   className="rounded-full h-12 w-12 object-cover"
                   src="/users/user-4.jpg"
@@ -216,11 +216,11 @@ export default async function Home() {
                   </div>
                 </div>
               </div> */}
-            </div>
-          </div>
-        </MaxWidthWrapper>
+        </div>
+        {/* </div> */}
+        {/* </MaxWidthWrapper> */}
 
-        <div className="pt-16">
+        <div>
           <Reviews />
         </div>
       </section>
