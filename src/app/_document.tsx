fa9d2f07f8da655082beeae8027fabc57cf047d7
Parjan <parjanhussain@gmail.com>
2024-11-01 18:14:20 +0530

    yy

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <script async src="https://tally.so/widgets/embed.js"></script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://tally.so/widgets/embed.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
