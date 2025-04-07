0dae79f33f8c6e5b40d5099422a61625327d3153
Subhambanik2004 <subhambanik2301@gmail.com>
2025-04-07 18:47:18 +0530

    updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,5 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+// export const GET = handleAuth();
+
+// route.ts
+export {}; // Makes it a valid module
